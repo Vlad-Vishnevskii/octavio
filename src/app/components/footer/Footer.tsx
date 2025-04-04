dafca98950dc0327108e6f289ff0a75973fc5ebe
Вишневский Влад <vishnevskii_vlad@mail.ru>
2025-04-04 12:28:09 +0300

    add footer

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -1,3 +1,51 @@
+import styles from './footer.module.scss';
+import Image from 'next/image';
+import Link from 'next/link';
+
 export const Footer = () => {
-  return <footer className="">footer</footer>;
+  return (
+    <footer className={styles.wrapper}>
+      <div className={styles.container}>
+        <div className={styles.top}>
+          <Image src="./octavio-logo-white.webp" width={200} height={57} alt="OCTAVIO" />
+
+          <nav className={styles.nav}>
+            <p className={styles.nav_title}>Инфо</p>
+            <Link className={styles.nav_item} href="/">
+              Главная
+            </Link>
+            <Link className={styles.nav_item} href="/blog">
+              Блог
+            </Link>
+            <Link className={styles.nav_item} href="/map">
+              Где купить?
+            </Link>
+          </nav>
+
+          <div className={styles.info}>
+            <p>Официальный дистрибьютер</p>
+            <p>ООО ТК “Технологика”</p>
+            <p>it-sound.ru</p>
+          </div>
+          <ul className={styles.contacts}>
+            <li className={styles.contacts_item}>
+              <a className={styles.contacts_link} href="tel:88007003334">
+                {'8 (800) 700-33-34'}
+              </a>
+            </li>
+            <li className={styles.contacts_item}>
+              <a className={styles.contacts_link} href="mailto:info@tl-sound.ru">
+                info@tl-sound.ru
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className={styles.bottom}>
+          <p>©Octavio 2025</p>
+          <p>Политика конфиденциальности</p>
+          <p>Design: Denis Vaschenko</p>
+        </div>
+      </div>
+    </footer>
+  );
 };
