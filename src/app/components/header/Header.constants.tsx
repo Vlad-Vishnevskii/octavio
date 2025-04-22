365fbe2e855be9e41caac256b7448dd208e8d9e8
Вишневский Влад <vishnevskii_vlad@mail.ru>
2025-04-22 19:52:10 +0300

    update header-bg

--- src/app/components/header/Header.constants.tsx
+++ src/app/components/header/Header.constants.tsx
@@ -1,5 +1,5 @@
 export const HEADER_IMG_PATHS = {
   LOGO: '/octavio-logo-white.webp',
   LOGO_BLACK: '/octavio-logo-blk.webp',
-  PRODUCT: '/header/header-product.png',
+  PRODUCT: '/Maestro-Black/5.webp',
 };
